9d663e4b65f395e17ef4e785fbb493725c16242e
Zina Mohamed <zina.a.mohamed@gmail.com>
2021-01-28 15:21:30 -0800

    tfirst stylefairy sweep🧚🏾‍♂️🏾‍♂️

--- data.js
+++ data.js
@@ -1,13 +1,86 @@
+const wand = {
+    id: 'wand',
+    title: 'A Trip to Ollivanders Wand Shop',
+    map: {
+        top: '17%',
+        left: '37%'
+    },
+    image: 'wand.gif',
+    background: 'alley.jpg',
+    audio: '',
+    description: `
+      "Good morning," Mr. Ollivander says, peering down at you. "We have important work to do. Are you ready to select the type of wand that will best suit you? Now tell me my dear, which of these creatures do you most fear?"
+  `,
+    choices: [{
+        id: 'basilisk',
+        description: 'The Basilisk',
+        result: `
+          Curious....very curious...looks like it'll be 10 3/4 inches made with the finest holly wood...and ahh yes very fitting....crafted with a phoenix feather core.
+      `,
+        wand: 'phoenix feather',
+    }, {
+        id: 'dementor',
+        description: 'Dementors',
+        result: ` 
+        Curious....very curious...looks like it'll be 14 inches made with the finest vine wood...and ahh yes very fitting....crafted with a dragon heartstring core.         
+      `,
+        wand: 'dragon heartstring',
+    }, {
+        id: 'Inferis',
+        description: 'Inferis',
+        result: `
+        Curious....very curious...looks like it'll be 12 inches made with the finest ash wood...and ahh yes very fitting....crafted with a unicorn hair core.  
+      `,
+        wand: 'unicorn hair',
+    }, {
+        id: 'acromantula',
+        description: 'Acromantula',
+        result: `
+        Curious....very curious...looks like it'll be 11 inches made with the finest hornbeam wood...and ahh yes very fitting....crafted with a horned serpent horn core. 
+      `,
+        wand: 'horned serpent core',
+    }]
+};
 
+const pet = {
+    id: 'pet',
+    title: 'A Trip to The Magical Menagerie',
+    map: {
+        top: '31%',
+        left: '5%'
+    },
+    image: 'pet.gif',
+    audio: '',
+    description: `
+    A pair of enormous purple toads sat gulping wetly and feasting on dead blowflies. It's time to pick a pet, as a matter of fact, it's time to let a pet pick you!
+  `,
+    choices: [{
+        id: 'owl',
+        description: 'Lorem Ipsum',
+        result: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+        pet: 'owl',
+
+    }, {
+        id: 'toad',
+        description: 'Lorem Ipsum',
+        result: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+        pet: 'toad',
+    }, {
+        id: 'cat',
+        description: 'Lorem Ipsum',
+        result: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+        pet: 'toad',
+    }]
+};
 
 const house = {
     id: 'house',
-    title: 'Sorting Hat Ceremony',
+    title: ' Your Sorting Hat Ceremony',
     map: {
         top: '89%',
         left: '44%'
     },
-    image: 'house.gif',
+    image: 'hat.gif',
     audio: '',
     description: `
     Are you afraid of what you'll hear?
@@ -49,84 +122,10 @@ const house = {
     }]
 };
 
-const wand = {
-    id: 'wand',
-    title: 'A Trip to Ollivanders Wand Shop',
-    map: {
-        top: '17%',
-        left: '37%'
-    },
-    image: 'wand.gif',
-    audio: '',
-    description: `
-      "Good morning," Mr. Ollivander says, peering down at you. "We have important work to do. Are you ready to select the type of wand that will best suit you? Now tell me my dear, which of these creatures do you most fear?"
-  `,
-    choices: [{
-        id: 'basilisk',
-        description: 'The Basilisk',
-        result: `
-          Curious....very curious...looks like it'll be 10 3/4 inches made with the finest holly wood...and ahh yes very fitting....crafted with a phoenix feather core.
-      `,
-        wand: 'phoenix feather',
-    }, {
-        id: 'dementor',
-        description: 'Dementors',
-        result: ` 
-        Curious....very curious...looks like it'll be 14 inches made with the finest vine wood...and ahh yes very fitting....crafted with a dragon heartstring core.         
-      `,
-        wand: 'dragon heartstring',
-    }, {
-        id: 'Inferis',
-        description: 'Inferis',
-        result: `
-        Curious....very curious...looks like it'll be 12 inches made with the finest ash wood...and ahh yes very fitting....crafted with a unicorn hair core.  
-      `,
-        wand: 'unicorn hair',
-    }, {
-        id: 'acromantula',
-        description: 'Acromantula',
-        result: `
-        Curious....very curious...looks like it'll be 11 inches made with the finest hornbeam wood...and ahh yes very fitting....crafted with a horned serpent horn core. 
-      `,
-        wand: 'horned serpent core',
-    }]
-};
-
-const pet = {
-    id: 'pet',
-    title: 'The Magical Menagerie',
-    map: {
-        top: '31%',
-        left: '5%'
-    },
-    image: 'pet.gif',
-    audio: '',
-    description: `
-    A pair of enormous purple toads sat gulping wetly and feasting on dead blowflies. It's time to pick a pet, as a matter of fact, it's time to let a pet pick you!
-  `,
-    choices: [{
-        id: 'owl',
-        description: 'Lorem Ipsum',
-        result: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
-        pet: 'owl',
-
-    }, {
-        id: 'toad',
-        description: 'Lorem Ipsum',
-        result: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
-        pet: 'toad',
-    }, {
-        id: 'cat',
-        description: 'Lorem Ipsum',
-        result: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
-        pet: 'toad',
-    }]
-};
-
 const quests = [
+    wand, 
+    pet,
     house, 
-    wand,
-    pet, 
 ];
 
 export default quests;
